8df240ee579b240c72464e6d07bd038af78b85de
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-01-27 19:36:51 +0100

    Add files via upload

---
+++ EJERCICIOS/Spring Data REST/backend-products/src/main/java/es/daw/springboot/backend/config/DataRestConfig.java
@@ -0,0 +1,24 @@
+package es.daw.springboot.backend.config;
+
+import es.daw.springboot.backend.entity.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // Exponer los IDs de las entidades en los endpoints de Spring Data REST
+        config.exposeIdsFor(Product.class);
+
+        // Configurar CORS globalmente para todos los endpoints de Spring Data REST
+        cors.addMapping("/**")  // Permite CORS para todas las rutas de Spring Data REST
+                .allowedOrigins("http://localhost:5173")  // Dominios permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*");  // Permite todos los encabezados
+    }
+}
+
+
---
+++ EJERCICIOS/Spring Data REST/backend-products/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO products (name, description, price) VALUES ('Producto 1', 'Descripción 1', 100);
+INSERT INTO products (name, description, price) VALUES ('Producto 2', 'Descripción 2', 200);
