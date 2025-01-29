fe13d4c419d1febf8d01fc6a774c771b263ed686
ProfeMelola <melola@educa.madrid.org>
2025-01-29 13:39:22 +0100

    actualizado

--- EJERCICIOS/Spring Data REST/backend-products/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products (name, description, price) VALUES ('Producto 1', 'Descripción 1', 100);
-INSERT INTO products (name, description, price) VALUES ('Producto 2', 'Descripción 2', 200);
