c1fb7b50378724b525c787faf4c85c4ebec3f021
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-02-08 14:38:52 +0100

    Delete EJERCICIOS/Spring Security y Authorization Server/MVC_Security directory
    deprecated

--- EJERCICIOS/Spring Security y Authorization Server/MVC_Security/src/main/java/es/daw/springsecurity/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package es.daw.springsecurity.repository;
-
-import es.daw.springsecurity.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
